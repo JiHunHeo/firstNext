ecb858fe6bd8fb17680e5e5c88805ed2aaf1569a
JiHunHeo <hyh6269@naver.com>
2023-11-04 11:45:25 +0900

    test commit

--- app/LogoutBtn.js
+++ app/LogoutBtn.js
@@ -1,21 +1,24 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 
 export default function LogoutBtn() {
     return (
-        <Button
-            href={`/`}
-            variant="primary"
-            onClick={() => {
-                signOut();
-            }}
-            style={{"width" : 180 + "px", 
-                    "height" : 50 + "px", 
-                    "vertical-align" : "middle",
-                    "padding" : 6 + "px " + 12 + "px"}}>
-            로그아웃
-        </Button>
+        <Link href={`/`}>
+            <Button
+                variant="primary"
+                onClick={() => {
+                    signOut();
+                }}
+                style={{"width" : 180 + "px", 
+                        "height" : 50 + "px", 
+                        "vertical-align" : "middle",
+                        "padding" : 6 + "px " + 12 + "px"}}>
+                로그아웃
+            </Button>
+        </Link>
+        
     );
 }
